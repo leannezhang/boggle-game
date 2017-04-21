cc19f4a05f4a30510a0e95327ba2865e62e4ef82
Eric Kim <bosung90@gmail.com>
2017-04-20 23:15:38 -0700

    Prettier fix data import

--- src/util/shuffleBoard.js
+++ src/util/shuffleBoard.js
@@ -1,23 +1,21 @@
-import {TileData} from '../../data'
+import { TileData } from '../data';
 
-const facesNum = 6
-const boardLength = 5
-const randomlySelectedFace = (faces) => {
-  let randomIndex = Math.floor(Math.random() * facesNum)
-  return faces.charAt(randomIndex)
-}
+const facesNum = 6;
+const boardLength = 5;
+const randomlySelectedFace = faces => {
+  let randomIndex = Math.floor(Math.random() * facesNum);
+  return faces.charAt(randomIndex);
+};
 
-
-const shuffleDice = (dice) => {
-  for (let i=0; i < dice.length; i++ ) {
-    let randomIndex = Math.floor(Math.random() *  dice.length) // random from 0 -> 25
+const shuffleDice = dice => {
+  for (let i = 0; i < dice.length; i++) {
+    let randomIndex = Math.floor(Math.random() * dice.length); // random from 0 -> 25
     let temp = dice[i];
-    dice[i] = dice[randomIndex]
+    dice[i] = dice[randomIndex];
     dice[randomIndex] = temp;
   }
-  return dice
-}
-
+  return dice;
+};
 
 export const shuffleBoard = () => {
   //  Create 1D array with dice
@@ -27,41 +25,60 @@ export const shuffleBoard = () => {
   //  Insert in the board and randomly pick a face
 
   const dice = [
-    "aaafrs", "aaeeee", "aafirs", "aeeeem", "aeegmu",
-    "aegmnn", "afirsy", "bjkqxz", "ccenst", "ceiilt",
-    "ceilpt", "ceipst", "ddhnot", "dhhlor", "dhhlor",
-    "dhlnor", "dhlnor", "eiiitt", "emottt", "ensssu",
-    "fiprsy", "gorrvw", "iprrry", "nootuw", "ooottu"
-  ]
+    'aaafrs',
+    'aaeeee',
+    'aafirs',
+    'aeeeem',
+    'aeegmu',
+    'aegmnn',
+    'afirsy',
+    'bjkqxz',
+    'ccenst',
+    'ceiilt',
+    'ceilpt',
+    'ceipst',
+    'ddhnot',
+    'dhhlor',
+    'dhhlor',
+    'dhlnor',
+    'dhlnor',
+    'eiiitt',
+    'emottt',
+    'ensssu',
+    'fiprsy',
+    'gorrvw',
+    'iprrry',
+    'nootuw',
+    'ooottu'
+  ];
 
   const board = [
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','','']
-  ]
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+  ];
 
   const shuffledDice = shuffleDice(dice);
 
-  for (let row=0; row < boardLength; row++) {
-    for(let col=0; col < boardLength; col++) {
-
-      let dice = shuffledDice.shift()
+  for (let row = 0; row < boardLength; row++) {
+    for (let col = 0; col < boardLength; col++) {
+      let dice = shuffledDice.shift();
 
       let face = randomlySelectedFace(dice);
       const tileData = new TileData(face, row, col);
       board[row][col] = tileData;
     }
   }
-  return board
-}
+  return board;
+};
 
 export const copyBoard = board => {
   const copiedBoard = board.map(row => {
     return row.map(tile => {
-      return tile.clone()
-    })
-  })
-  return copiedBoard
-}
+      return tile.clone();
+    });
+  });
+  return copiedBoard;
+};
